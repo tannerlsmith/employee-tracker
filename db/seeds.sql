16f1afc1ff50b85f5c2943c132f40fb2cfe1008e
tannerlsmith <tanner.smith@uvm.edu>
2021-07-17 21:30:17 -0400

    added seeds.sql with some info

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,15 @@ VALUES
     ('engineer'),
     ('legal'),
     ('finance');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Leader', 30000, 100),
+    ('Sales Associate', 24000, 110),
+    ('Senior Engineer', 40000, 200),
+    ('Engineer', 35000, 210),
+    ('Financial Analyst', 50000, 400),
+    ('Financial Planner', 45000, 410),
+    ('Lawyer', 60000, 300),
+    ('Paralegal', 55000, 310');
