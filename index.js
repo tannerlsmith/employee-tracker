07609029a6ebe2e9051b193ceb6afc684dd92b8d
tannerlsmith <tanner.smith@uvm.edu>
2021-07-26 17:59:09 -0400

    added 'create employee' function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require ('inquirer')
-// Needs to connect class(11)
 require('console.table')
 const connection = require ('./db/connection')
+const addEmployeeQuestions = ['What is the first name?', 'What is the last name?', 'What is their role?', 'Who is their manager?']
+
 
 function menu() {
     inquirer.prompt([
@@ -59,21 +60,42 @@ function menu() {
             viewEmployees()
         }
 
+        else if (answers.choice === 'addDepartment') {
+            addDepartment()
+        }
+
+        else if (answers.choice === 'addEmployee') {
+            addEmployee()
+        }
+
         else {
             console.log('Goodbye.')
             connection.end()
         }
-
-
     })
 }
 
-function viewEmployees() {
-    var queryEmployees = 'SELECT * FROM employee;'
-    connection.query(queryEmployees, (err, data) => {
-        if (err) throw err;
-        console.table(data)
-        menu();
+// ===============FUNCTIONS=============== //
+function addDepartment() { 
+    inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "What Department would you like to add?"
+        }
+    ]).then(function(res) {
+        var addDeptInq = connection.query(
+            "INSERT INTO department SET ?",
+            {
+              name: res.name
+            
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                menu();
+            }
+        )
     })
 }
 
@@ -95,19 +117,80 @@ function viewDepartments() {
     })
 }
 
+function viewEmployees() {
+    var queryEmployees = 'SELECT * FROM employee;'
+    connection.query(queryEmployees, (err, data) => {
+        if (err) throw err;
+        console.table(data)
+        menu();
+    })
+}
+
+
+// WORKING ON THIS
+function addEmployee() {
+    connection.query('SELECT * FROM role', function (err, res) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: 'first_name',
+                    type: 'input', 
+                    message: "What is the employee's fist name? ",
+                },
+                {
+                    name: 'last_name',
+                    type: 'input', 
+                    message: "What is the employee's last name? "
+                },
+                {
+                    name: 'manager_id',
+                    type: 'input', 
+                    message: "What is the employee's manager's ID? "
+                },
+                {
+                    name: 'role', 
+                    type: 'list',
+                    choices: function() {
+                    var roleArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                        roleArray.push(res[i].title);
+                    }
+                    return roleArray;
+                    },
+                    message: "What is this employee's role? "
+                }
+                ]).then(function (answer) {
+                    let role_id;
+                    for (let a = 0; a < res.length; a++) {
+                        if (res[a].title == answer.role) {
+                            role_id = res[a].id;
+                            console.log(role_id)
+                        }                  
+                    }  
+                    connection.query(
+                    'INSERT INTO employee SET ?',
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        manager_id: answer.manager_id,
+                        role_id: role_id,
+                    },
+                    function (err) {
+                        if (err) throw err;
+                        console.log('Your employee has been added!');
+                        menu();
+                    })
+                })
+        })
+};
+
+
+
+// NEW FUNCTIONS
+function addRole() {}
+function updateEmployeeRole() {}
+
+
+
 menu()
-
-// at the end create option called exit
-
-// name: = menu
-// .query()
-// select alal from department = SELECT * FROM department
-// console.table makes it look like table in terminal
-
-
-// NEXT STEPS: 
-// add departments
-// add roles
-// add employees
-
-// use terminal commands 
